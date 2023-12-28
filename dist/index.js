1fc5490145a3063e52ff5fc55145116100f424fc
Achraf <achrafbouchtarouif@gmail.com>
2023-12-28 11:01:30 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -23529,7 +23529,7 @@ async function commitChanges(lang) {
     `docs: Added ${model}_${lang}.md translation`
   );
   console.log("finished commit");
-  await git.raw(['config', 'pull.ff', 'only']);
+  await git.raw(['config', 'pull.rebase', 'true']);
   await git.pull();
   console.log("pulled");
   await git.push();
